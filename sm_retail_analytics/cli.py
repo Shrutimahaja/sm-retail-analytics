435658afd986019e096a8c6200048dcb0916eb07
Shrutimahaja <Shrutimahaja@users.noreply.github.com>
2024-03-20 20:21:46 +0000

    ✅ Ready to clone and code.

--- sm_retail_analytics/cli.py
+++ sm_retail_analytics/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for sm_retail_analytics project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m sm_retail_analytics` and `$ sm_retail_analytics `.
 
     This is your program's entry point.
 
